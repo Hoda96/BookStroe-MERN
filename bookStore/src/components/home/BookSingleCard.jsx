5584de277016041651a7518334ca6159984a15fc
Hoda96 <farahani.h40@gmail.com>
2024-11-01 20:19:31 +0330

    add book Modal

--- bookStore/src/components/home/BookSingleCard.jsx
+++ bookStore/src/components/home/BookSingleCard.jsx
@@ -5,6 +5,7 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { useState } from "react";
+import BookModal from "./BookModal";
 
 function BookSingleCard({ book }) {
   const [showModal, setShowModal] = useState(false);
@@ -37,9 +38,9 @@ function BookSingleCard({ book }) {
           <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
         </Link>
       </div>
-      {/* {showModal && (
+      {showModal && (
         <BookModal book={book} onClose={() => setShowModal(false)} />
-      )} */}
+      )}
     </div>
   );
 }
